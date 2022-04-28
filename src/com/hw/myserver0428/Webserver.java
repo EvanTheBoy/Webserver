976417f7a4335a5629fa3ee33298a95bb9836d55
Evan <761356554@qq.com>
2022-04-28 20:05:47 +0800

    Update Webserver.java

--- src/com/hw/myserver0428/Webserver.java
+++ src/com/hw/myserver0428/Webserver.java
@@ -1,7 +1,22 @@
 package com.hw.myserver0428;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Webserver {
+    public void createServer(int port) {
+        try {
+            ServerSocket ss = new ServerSocket(port);
+            Socket s = ss.accept();
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     public static void main(String[] args) {
-        
+
     }
 }
