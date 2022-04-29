ee64271f872dc7740c44b5b40a8d298401fa35db
Evan <761356554@qq.com>
2022-04-29 21:44:30 +0800

    Update Response.java

--- src/com/hw/myserver0428/Response.java
+++ src/com/hw/myserver0428/Response.java
@@ -1,4 +1,20 @@
 package com.hw.myserver0428;
 
+import java.io.IOException;
+import java.io.OutputStream;
+
 public class Response {
+    private OutputStream output;
+    public Response(OutputStream output) {
+        this.output = output;
+    }
+
+    public void write(String msg) {
+        try {
+            output.write(msg.getBytes());
+            output.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
