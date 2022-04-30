86529b967973c5d8fcf84a4799c97a50024b9662
Evan <761356554@qq.com>
2022-04-30 16:51:23 +0800

    Updates

--- src/com/hw/myserver0428/ServerThread.java
+++ src/com/hw/myserver0428/ServerThread.java
@@ -20,22 +20,35 @@ public class ServerThread implements Runnable {
             String[] lineArr = line.split(" "); //根据空格切割
             String fileName = lineArr[1].substring(1); //下面这个fileName就是index.html
             //根据文件名从服务器的文件系统获取文件，其实也就是从磁盘读取文件。
-            FileInputStream fis = new FileInputStream(fileName);
-            BufferedInputStream bis = new BufferedInputStream(fis); //我们还是转换成缓冲流以提高效率
-            byte[] bytes = new byte[1024];
-            BufferedOutputStream bos = new BufferedOutputStream(socket.getOutputStream());
-            int length = 0;
-            //由请求文件组成的HTTP响应报文，报文前面有首部行
-            String head = "HTTP/1.0 200 OK \r\n"
-                    + "Content-Type:text/html \r\n"
-                    + "\r\n";
-            bos.write(head.getBytes(StandardCharsets.UTF_8)); //先把首部行发出去，再发数据
-            //现在发数据,向请求浏览器发送响应
-            while ((length = bis.read(bytes)) != -1) {
-                bos.write(bytes, 0, length);
+            File file = new File(fileName);
+            if (file.exists()) {
+                FileInputStream fis = new FileInputStream(fileName);
+                BufferedInputStream bis = new BufferedInputStream(fis); //我们还是转换成缓冲流以提高效率
+                byte[] bytes = new byte[1024];
+                BufferedOutputStream bos = new BufferedOutputStream(socket.getOutputStream());
+                int length = 0;
+                //由请求文件组成的HTTP响应报文，报文前面有首部行
+                String head = "HTTP/1.0 200 OK \r\n"
+                        + "Content-Type:text/html \r\n"
+                        + "\r\n";
+                bos.write(head.getBytes()); //先把首部行发出去，再发数据
+                //现在发数据,向请求浏览器发送响应
+                while ((length = bis.read(bytes)) != -1) {
+                    bos.write(bytes, 0, length);
+                }
+                bos.close();
+                bis.close();
+            } else {
+                //请求文件不存在，就产生404页面
+                BufferedOutputStream bos = new BufferedOutputStream(socket.getOutputStream());
+                String notFound = "<html><h1>404 Not Found</h1></html>";
+                String head = "HTTP/1.0 404 no found \r\n"
+                        + "Content-Type:text/html \r\n"
+                        + "\r\n";
+                bos.write(head.getBytes());
+                bos.write(notFound.getBytes(StandardCharsets.UTF_8));
+                bos.close();
             }
-            bos.close();
-            bis.close();
             bReader.close();
             socket.close();
         } catch (Exception e) {
